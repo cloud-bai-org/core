706b894a9b1f9b76fd987d4a0ef652a5c065b3d7
yu <chuwork.99@gmail.com>
2026-03-28 17:29:41 +0800

    chore: 初始化 Nuxt 4 專案
    使用 nuxi init（minimal template）建立 Nuxt 4.4.2 專案，
    設定 package.json、tsconfig.json、nuxt.config.ts，
    更新 .gitignore 加入 .nuxt/ 和 .output/

---
+++ nuxt.config.ts
@@ -0,0 +1,65 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    'shadcn-nuxt',
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate/nuxt',
+    '@vueuse/motion/nuxt',
+    '@vite-pwa/nuxt',
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  shadcn: {
+    prefix: '',
+    componentDir: './app/components/ui',
+  },
+
+  pwa: {
+    manifest: {
+      name: '線上拜拜',
+      short_name: '線上拜拜',
+      description: '將華人傳統拜拜習俗數位化，以環保方式完成儀式',
+      lang: 'zh-TW',
+      theme_color: '#1C1919',
+      background_color: '#1C1919',
+      display: 'standalone',
+      icons: [
+        {
+          src: '/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: '/',
+    },
+  },
+
+  // TODO: 待 shadcn-vue SSR 相容性穩定後，可將 / 改為 prerender、/eco-impact 改為 SSR
+  ssr: false,
+
+  routeRules: {
+    '/': { prerender: true },
+    '/login': { ssr: false },
+    '/settings': { ssr: false },
+    '/worship/**': { ssr: false },
+    '/temple/**': { ssr: false },
+    '/group/**': { ssr: false },
+  },
+
+  css: ['~/assets/css/main.css'],
+})
